b56c6f31de995fb4cab10050fa196fcb3673f90d
Duncan Tebbs <duncan.tebbs@gmail.com>
2020-09-07 15:40:25 +0100

    fully working package

--- src/libff.ts
+++ src/libff.ts
@@ -1,3 +1,6 @@
 // Copyright (c) 2015-2020 Clearmatics Technologies Ltd
 //
 // SPDX-License-Identifier: LGPL-3.0+
+
+export var bw6_761 = require('./bw6_761')
+export var bls12_377 = require('./bls12_377')
