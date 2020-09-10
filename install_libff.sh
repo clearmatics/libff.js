425e7844ec537a189a44b2893840db97dc598063
Duncan Tebbs <duncan.tebbs@gmail.com>
2020-09-10 11:12:37 +0100

    fix ci workflow

--- install_libff.sh
+++ install_libff.sh
@@ -7,11 +7,15 @@ if [ "$VERSION" == "" ] ; then
     VERSION=ffi
 fi
 
+platform=`uname`
+
 echo "================================"
 echo "===== INSTALLING LIBFF-FFI ====="
 echo "================================"
 echo version: $VERSION
+echo platform: ${platform}
 
+# If necessary, manually sync the libff repo at the correct version
 if ! [ -e depends/libff ] ; then
     echo "depends/libff not present. downloading version: $VERSION"
     mkdir -p depends
@@ -23,13 +27,19 @@ if ! [ -e depends/libff ] ; then
     popd
 fi
 
-mkdir -p bin
-pushd depends/libff
+# On macos, ensure openssl libraries can be found by cmake
+if [ "${platform}" == "Darwin" ]; then
+    openssl_path=$(brew --prefix openssl)
+    export PKG_CONFIG_PATH="${openssl_path}/lib/pkgconfig"
+fi
 
-  mkdir -p build
-  pushd build
+# Create the destination directory
+mkdir -p bin
+
+# Build and install the libff-ffi shared library
+mkdir -p depends/libff/build
+pushd depends/libff/build
     cmake -DWITH_PROCPS=OFF ..
     make -j 5 ff-ffi
     rsync --update ffi/libff-ffi.* ../../../bin
-  popd
 popd
