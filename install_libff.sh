48b585f30362f033849034bcc1345f0978b8eb31
Duncan Tebbs <duncan.tebbs@gmail.com>
2020-09-10 10:17:28 +0100

    improve install script

--- install_libff.sh
+++ install_libff.sh
@@ -2,22 +2,34 @@
 
 set -e
 
+VERSION=$1
+if [ "$VERSION" == "" ] ; then
+    VERSION=ffi
+fi
+
 echo "================================"
 echo "===== INSTALLING LIBFF-FFI ====="
 echo "================================"
+echo version: $VERSION
 
-git submodule update --init --recursive
+if ! [ -e depends/libff ] ; then
+    echo "depends/libff not present. downloading version: $VERSION"
+    mkdir -p depends
+    pushd depends
+        git clone -b $VERSION https://github.com/clearmatics/libff
+        pushd libff
+            git submodule update --init --recursive
+        popd
+    popd
+fi
+
 mkdir -p bin
-
 pushd depends/libff
 
   mkdir -p build
   pushd build
-
     cmake -DWITH_PROCPS=OFF ..
     make -j 5 ff-ffi
     rsync --update ffi/libff-ffi.* ../../../bin
-
   popd
-
 popd
